fc4fe008aa62337444863af86dad1b8fbc59d4c1
Jesus Romero <elherzt@gmail.com>
2015-08-24 10:36:51 -0500

    add photos to dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,6 @@
 class DogsController < ApplicationController
-  before_action :set_dog, only: [:show, :edit, :update, :destroy, :select_location, :new, :create]
+  before_action :set_dog, only: [:show, :edit, :update, :destroy, :select_location]
+  before_action :set_user, only: [:new, :create]
   before_action :authenticate_user!, only: [:create, :new, :edit]
 
   # GET /dogs
@@ -26,18 +27,16 @@ class DogsController < ApplicationController
   # POST /dogs.json
   def create
     @dog = @user.dogs.new(dog_params)
-    respond_to do |format|
       if @dog.save
         #if params[:photos]
           #params[:photos].first.each do |photo|
             #@dog.photos.create(photo: photo[1])
           #end
         #end
-        format.html { redirect_to new_user_dog_photo_path(@user, @dog) }
+        redirect_to new_user_dog_photo_path(@user, @dog)
       else
-        format.html { render :new }
+        render :new
       end
-    end
   end
 
   # DELETE /dogs/1
@@ -62,6 +61,10 @@ class DogsController < ApplicationController
     def set_dog
       @user = current_user
       @dog = Dog.find(params[:id])
+    end
+
+    def set_user
+      @user = current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
